b7f4c17de64407d878391e58698b3ca0804ecccc
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-06-17 18:56:04 +0530

    feat: add copy URL functionality and improve URL display in Dashboard

--- url-shortener/frontend/src/pages/Dashboard.jsx
+++ url-shortener/frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
     const [urls, setUrls] = useState([]);
+    const [copied,setCopied] = useState(false);
     const navigate = useNavigate();
     useEffect(() => {
         const fetchUrls = async () => {
@@ -27,6 +28,17 @@ function Dashboard() {
             console.error('Error deleting URL:', error);
         }
     }
+
+    const handleCopy = async(shortCode) => {
+        try{
+            await navigator.clipboard.writeText(`http://localhost:3000/${shortCode}`);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000);
+        }
+        catch (error) {
+            console.error('Error copying URL:', error);
+        }
+    }
   return (
     <div className="bg-slate-100 min-h-screen">
       <div className="text-center p-4 text-2xl font-bold pt-6 text-slate-800">
@@ -46,26 +58,38 @@ function Dashboard() {
             className="border border-slate-200 shadow-md m-4 p-4 rounded-lg bg-white"
           >
             <p className="text-lg font-semibold mb-4">{url.originalUrl}</p>
-            
+
             <p className="text-md font-medium">Short Code: {url.shortCode}</p>
             <p className="text-lg font-bold">
-              Short URL: <span className="text-blue-600">{`http://localhost:3000/${url.shortCode}`}</span>
+              Short URL:{" "}
+              <span className={`text-blue-600 cursor-pointer ${copied ? "bg-blue-200" : ""}`}>
+                <a href={`http://localhost:3000/${url.shortCode}`} target="_blank">
+                  {`http://localhost:3000/${url.shortCode}`}
+                </a>
+              </span>
             </p>
-            
+
             <p className="text-md font-medium mt-4">Clicks: {url.clicks}</p>
             <div className="flex gap-4 mt-2">
-              <button className="px-4 py-2 rounded-lg bg-blue-600 hover:bg-blue-700 text-white hover:cursor-pointer"
-              onClick={() => navigate(`/editurl/${url.shortCode}`)}
+              <button
+                className="px-4 py-2 rounded-lg bg-blue-600 hover:bg-blue-700 text-white hover:cursor-pointer"
+                onClick={() => navigate(`/editurl/${url.shortCode}`)}
               >
                 Edit Url
               </button>
-              <button className="px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 text-white hover:cursor-pointer"
-              onClick={() => deleteUrl(url.shortCode)}
+              <button
+                className="px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 text-white hover:cursor-pointer"
+                onClick={() => deleteUrl(url.shortCode)}
               >
                 Delete Url
               </button>
-              <button className="px-4 py-2 rounded-lg bg-green-600 hover:bg-green-700 text-white hover:cursor-pointer">
-                Copy Shortened Url
+              <button
+                className={`px-4 py-2 rounded-lg  text-white hover:cursor-pointer 
+              ${copied ? "bg-green-800" : "bg-green-600 hover:bg-green-700"}
+              `}
+                onClick={() => handleCopy(url.shortCode)}
+              >
+                {copied ? "Copied!" : "Copy Shortened Url"}
               </button>
             </div>
           </div>
