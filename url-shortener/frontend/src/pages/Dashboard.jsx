4a5b0dc4fdbb8b15fe1411617d2a786615139f3e
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-06-17 17:38:12 +0530

    feat: add delete functionality and navigation for editing URLs in Dashboard

--- url-shortener/frontend/src/pages/Dashboard.jsx
+++ url-shortener/frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
     const [urls, setUrls] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         const fetchUrls = async () => {
             try{
@@ -15,6 +17,16 @@ function Dashboard() {
         }
         fetchUrls();
     },[])
+
+    const deleteUrl = async (shortCode) => {
+        try{
+            await axios.delete(`http://localhost:3000/api/urls/delete/${shortCode}`);
+            const updatedUrls = urls.filter((url) => url.shortCode !== shortCode);
+            setUrls(updatedUrls);
+        }catch (error) {
+            console.error('Error deleting URL:', error);
+        }
+    }
   return (
     <div className="bg-slate-100 min-h-screen">
       <div className="text-center p-4 text-2xl font-bold pt-6 text-slate-800">
@@ -40,10 +52,14 @@ function Dashboard() {
             
             <p className="text-md font-medium mt-4">Clicks: {url.clicks}</p>
             <div className="flex gap-4 mt-2">
-              <button className="px-4 py-2 rounded-lg bg-blue-600 hover:bg-blue-700 text-white hover:cursor-pointer">
+              <button className="px-4 py-2 rounded-lg bg-blue-600 hover:bg-blue-700 text-white hover:cursor-pointer"
+              onClick={() => navigate(`/editurl/${url.shortCode}`)}
+              >
                 Edit Url
               </button>
-              <button className="px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 text-white hover:cursor-pointer">
+              <button className="px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 text-white hover:cursor-pointer"
+              onClick={() => deleteUrl(url.shortCode)}
+              >
                 Delete Url
               </button>
               <button className="px-4 py-2 rounded-lg bg-green-600 hover:bg-green-700 text-white hover:cursor-pointer">
