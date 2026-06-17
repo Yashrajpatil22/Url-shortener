25c0b7079104496eb98e9d7006da5b63f7e450c7
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-06-17 16:07:51 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS

--- url-shortener/backend/src/index.js
+++ url-shortener/backend/src/index.js
@@ -2,12 +2,18 @@ import express from "express";
 import connectDb from "./db/index.js";
 import urlRoutes from "./routes/url.route.js";
 import { redirectToOriginalUrl } from "./controllers/url.controller.js";
+import cors from "cors";
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use("/api/urls", urlRoutes);
 app.get("/:shortCode", redirectToOriginalUrl);
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+  }
+));
 
 app.get("/check", (req, res) => {
   res.send("Hello World");
